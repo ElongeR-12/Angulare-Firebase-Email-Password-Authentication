57787b55e2c904cdff4f6378bb113932429ebe24
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2019-12-22 23:25:54 +0100

    configure routes path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserLoginComponent } from './auth/user-login/user-login.component';
+import { UserInfoComponent } from './auth/user-info/user-info.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: UserLoginComponent},
+  {path: 'user', component: UserInfoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
