596103a2f9345d2fdb00a3b528ca11a848838a67
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2019-12-22 23:58:08 +0100

    inject private AngularFireAuth and private router in service constructor

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private router: Router) { }
 }
